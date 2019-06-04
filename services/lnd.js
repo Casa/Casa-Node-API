c1cbf00d53657ef44aff8b5a4e11a5f8ef4a07c2
Michael Borglin <michael@team.casa>
2019-06-04 13:01:00 -0700

    update node api

---
+++ services/lnd.js
@@ -0,0 +1,392 @@
+/* eslint-disable camelcase, max-lines */
+const grpc = require('grpc');
+const camelizeKeys = require('camelize-keys');
+
+const diskService = require('services/disk');
+const LndError = require('models/errors.js').LndError;
+
+const LND_HOST = '127.0.0.1';
+const TLS_FILE = process.env.TLS_FILE || '/lnd/tls.cert';
+const PROTO_FILE = process.env.PROTO_FILE || './resources/rpc.proto';
+const LND_PORT = process.env.LND_PORT || 10009; // eslint-disable-line no-magic-numbers
+
+// LND changed the macaroon path to ~/.lnd/data/chain/{chain}/{network}/admin.macaroon. We are currently only
+// supporting bitcoind and have that hard coded. However, we are leaving the ability to switch between testnet and
+// mainnet. This can be done with the /reset route. LND_NETWORK will be defaulted in /usr/local/casa/applications/.env.
+// LND_NETWORK will be overwritten in the settings file.
+let MACAROON_FILE = '/lnd/data/chain/bitcoin/' + process.env.LND_NETWORK + '/admin.macaroon';
+
+// Developers should overwrite MACAROON_DIR in their .env file or ide. We recommend 'os.homedir() + /lightning-node/'.
+if (process.env.MACAROON_DIR) {
+  MACAROON_FILE = process.env.MACAROON_DIR + 'admin.macaroon';
+}
+
+// TODO move this to volume
+const lnrpcDescriptor = grpc.load(PROTO_FILE);
+const lnrpc = lnrpcDescriptor.lnrpc;
+
+const DEFAULT_RECOVERY_WINDOW = 250;
+
+// Initialize RPC client will attempt to connect to the lnd rpc with a tls.cert and admin.macaroon. If the wallet has
+// not bee created yet, then the client will only be initialized with the tls.cert. There may be times when lnd wallet
+// is reset and the tls.cert and admin.macaroon will change.
+async function initializeRPCClient() {
+  return diskService.readFile(TLS_FILE)
+    .then(lndCert => {
+      const sslCreds = grpc.credentials.createSsl(lndCert);
+
+      return diskService.readFile(MACAROON_FILE)
+        .then(macaroon => {
+          // build meta data credentials
+          const metadata = new grpc.Metadata();
+          metadata.add('macaroon', macaroon.toString('hex'));
+          const macaroonCreds = grpc.credentials.createFromMetadataGenerator((_args, callback) => {
+            callback(null, metadata);
+          });
+
+          // combine the cert credentials and the macaroon auth credentials
+          // such that every call is properly encrypted and authenticated
+          return {
+            credentials: grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds),
+            state: true
+          };
+        })
+        .catch(() => ({credentials: sslCreds, state: 'WALLET_CREATION_ONLY'}));
+    })
+    .then(({credentials, state}) => ({
+      lightning: new lnrpc.Lightning(LND_HOST + ':' + LND_PORT, credentials),
+      walletUnlocker: new lnrpc.WalletUnlocker(LND_HOST + ':' + LND_PORT, credentials),
+      state: state // eslint-disable-line object-shorthand
+    }));
+}
+
+async function promiseify(rpcObj, rpcFn, payload, description) {
+  return new Promise((resolve, reject) => {
+    try {
+      rpcFn.call(rpcObj, payload, (error, grpcResponse) => {
+        if (error) {
+          reject(new LndError(`Unable to ${description}`, error));
+        } else {
+          resolve(camelizeKeys(grpcResponse, '_'));
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// an amount, an options memo, and can only be paid to node that created it.
+function addInvoice(amount, memo) {
+  const rpcPayload = {
+    value: amount,
+    memo: memo, // eslint-disable-line object-shorthand
+    expiry: 3600 // Should we make this ENV specific for ease of testing?
+  };
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.addInvoice, rpcPayload, 'create new invoice'))
+    .then(grpcResponse => {
+      if (grpcResponse && grpcResponse.paymentRequest) {
+        return grpcResponse.paymentRequest;
+      } else {
+        throw new LndError('Unable to parse invoice from lnd');
+      }
+    });
+}
+
+function closeChannel(fundingTxId, index, force) {
+  const rpcPayload = {
+    channel_point: {
+      funding_txid_str: fundingTxId,
+      output_index: index
+    },
+    force: force // eslint-disable-line object-shorthand
+  };
+
+  return initializeRPCClient().then(({lightning}) => new Promise((resolve, reject) => {
+    try {
+      const call = lightning.CloseChannel(rpcPayload);
+
+      call.on('data', chan => {
+        if (chan.update === 'close_pending') {
+          resolve();
+        }
+      });
+
+      call.on('error', error => {
+        reject(new LndError('Unable to close channel', error));
+      });
+    } catch (error) {
+      reject(error);
+    }
+  }));
+}
+
+// Connects this lnd node to a peer.
+function connectToPeer(pubKey, ip, port) {
+  const rpcPayload = {
+    addr: {
+      pubkey: pubKey,
+      host: ip + ':' + port
+    }
+  };
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ConnectPeer, rpcPayload, 'connect to peer'));
+}
+
+function decodePaymentRequest(paymentRequest) {
+  const rpcPayload = {
+    pay_req: paymentRequest
+  };
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.decodePayReq, rpcPayload, 'decode payment request'))
+    .then(invoice => {
+      // add on payment request for extra details
+      invoice.paymentRequest = paymentRequest;
+
+      return invoice;
+    });
+}
+
+async function estimateFee(address, amt, confTarget) {
+  const addrToAmount = {};
+  addrToAmount[address] = amt;
+
+  const rpcPayload = {
+    AddrToAmount: addrToAmount,
+    target_conf: confTarget,
+  };
+
+  const conn = await initializeRPCClient();
+
+  return await promiseify(conn.lightning, conn.lightning.estimateFee, rpcPayload, 'estimate fee request');
+}
+
+function generateAddress() {
+  const rpcPayload = {
+    type: 1
+  };
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.NewAddress, rpcPayload, 'generate address'));
+}
+
+function generateSeed() {
+  return initializeRPCClient().then(({walletUnlocker, state}) => {
+    if (state === true) {
+      throw new LndError('Macaroon exists, therefor wallet already exists');
+    }
+
+    return promiseify(walletUnlocker, walletUnlocker.GenSeed, {}, 'generate seed');
+  });
+}
+
+function getChannelBalance() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ChannelBalance, {}, 'get channel balance'));
+}
+
+function getForwardingEvents(startTime, endTime, indexOffset) {
+  const rpcPayload = {
+    start_time: startTime,
+    end_time: endTime,
+    index_offset: indexOffset,
+  };
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ForwardingHistory, rpcPayload, 'get forwarding events'));
+}
+
+function getInfo() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.GetInfo, {}, 'get lnd information'));
+}
+
+// Returns a list of lnd's currently open channels. Channels are considered open by this node and it's directly
+// connected peer after three confirmation. After six confirmations, the channel is broadcasted by this node and it's
+// directly connected peer to the broader lightning network.
+function getOpenChannels() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ListChannels, {}, 'list channels'))
+    .then(grpcResponse => grpcResponse.channels);
+}
+
+function getClosedChannels() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ClosedChannels, {}, 'closed channels'))
+    .then(grpcResponse => grpcResponse.channels);
+}
+
+// Returns a list of all outgoing payments.
+function getPayments() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ListPayments, {}, 'get payments'));
+}
+
+// Returns a list of all lnd's currently connected and active peers.
+function getPeers() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ListPeers, {}, 'get peer information'))
+    .then(grpcResponse => {
+      if (grpcResponse && grpcResponse.peers) {
+        return grpcResponse.peers;
+      } else {
+        throw new LndError('Unable to parse peer information');
+      }
+    });
+}
+
+// Returns a list of lnd's currently pending channels. Pending channels include, channels that are in the process of
+// being opened, but have not reached three confirmations. Channels that are pending closed, but have not reached
+// one confirmation. Forced close channels that require potentially hundreds of confirmations.
+function getPendingChannels() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.PendingChannels, {}, 'list pending channels'));
+}
+
+function getWalletBalance() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.WalletBalance, {}, 'get wallet balance'));
+}
+
+function initWallet(options) {
+  const passwordBuff = Buffer.from(options.password, 'utf8');
+
+  const rpcPayload = {
+    wallet_password: passwordBuff,
+    cipher_seed_mnemonic: options.mnemonic,
+    recovery_window: DEFAULT_RECOVERY_WINDOW
+  };
+
+  return initializeRPCClient().then(({walletUnlocker, state}) => {
+    if (state === true) {
+      throw new LndError('Macaroon exists, therefor wallet already exists');
+    }
+
+    return promiseify(walletUnlocker, walletUnlocker.InitWallet, rpcPayload, 'initialize wallet')
+      .then(() => options.mnemonic);
+  });
+}
+
+// Returns a list of all invoices.
+function getInvoices() {
+  const rpcPayload = {
+    reversed: true, // Returns most recent
+    num_max_invoices: 100,
+  };
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.ListInvoices, rpcPayload, 'list invoices'));
+}
+
+// Returns a list of all on chain transactions.
+function getOnChainTransactions() {
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.GetTransactions, {}, 'list on-chain transactions'))
+    .then(grpcResponse => grpcResponse.transactions);
+}
+
+async function listUnspent() {
+  const rpcPayload = {
+    min_confs: 0,
+    max_confs: 10000000, // Use arbitrarily high maximum confirmation limit.
+  };
+
+  const conn = await initializeRPCClient();
+
+  return await promiseify(conn.lightning, conn.lightning.listUnspent, rpcPayload, 'estimate fee request');
+}
+
+function openChannel(pubKey, amt, satPerByte) {
+  const rpcPayload = {
+    node_pubkey_string: pubKey,
+    local_funding_amount: amt,
+  };
+
+  if (satPerByte) {
+    rpcPayload.sat_per_byte = satPerByte;
+  } else {
+    rpcPayload.target_conf = 6;
+  }
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.OpenChannelSync, rpcPayload, 'open channel'));
+}
+
+function sendCoins(addr, amt, satPerByte, sendAll) {
+  const rpcPayload = {
+    addr: addr, // eslint-disable-line object-shorthand
+    amount: amt,
+    send_all: sendAll,
+  };
+
+  if (satPerByte) {
+    rpcPayload.sat_per_byte = satPerByte;
+  } else {
+    rpcPayload.target_conf = 6;
+  }
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.SendCoins, rpcPayload, 'send coins'));
+}
+
+function sendPaymentSync(paymentRequest, amt) {
+  const rpcPayload = {
+    payment_request: paymentRequest,
+    amt: amt,
+  };
+
+  return initializeRPCClient()
+    .then(({lightning}) => promiseify(lightning, lightning.SendPaymentSync, rpcPayload, 'send lightning payment'))
+    .then(response => {
+      // sometimes the error comes in on the response...
+      if (response.paymentError) {
+        throw new LndError(`Unable to send lightning payment: ${response.paymentError}`);
+      }
+
+      return response;
+    });
+}
+
+function unlockWallet(password) {
+  const passwordBuff = Buffer.from(password, 'utf8');
+
+  const rpcPayload = {
+    wallet_password: passwordBuff
+  };
+
+  // TODO how to determine if wallet is already unlocked?
+  // This will throw code 12 unimplemented, which is not very helpful
+  return initializeRPCClient()
+    .then(({walletUnlocker}) => promiseify(walletUnlocker, walletUnlocker.UnlockWallet, rpcPayload, 'unlock wallet'));
+}
+
+module.exports = {
+  addInvoice,
+  closeChannel,
+  connectToPeer,
+  decodePaymentRequest,
+  estimateFee,
+  getChannelBalance,
+  getClosedChannels,
+  getForwardingEvents,
+  getInfo,
+  getInvoices,
+  getOpenChannels,
+  getPayments,
+  getPeers,
+  getPendingChannels,
+  getWalletBalance,
+  generateAddress,
+  generateSeed,
+  getOnChainTransactions,
+  initWallet,
+  listUnspent,
+  openChannel,
+  sendCoins,
+  sendPaymentSync,
+  unlockWallet,
+};
---
+++ test/mocks/lnd.js
@@ -0,0 +1,501 @@
+/* eslint-disable camelcase, id-length, max-len */
+
+function generateAddress() {
+  return '2NFGwqm9N9LomEh9mzQgofr1WGqkwaxPuWg';
+}
+
+function getChannelBalance() {
+  return 42000;
+}
+
+function getEstimateFee() {
+  return {
+    feeSat: '44115',
+    feerateSatPerByte: '83',
+  };
+}
+
+function getForwardingEvents() {
+  return {
+    forwardingEvents: [
+      {
+        timestamp: '1548021945',
+        chanIdIn: '614599512239964161',
+        chanIdOut: '614438983628095489',
+        amtIn: '2',
+        amtOut: '1',
+        fee: '1'
+      }
+    ],
+    lastOffsetIndex: 1,
+  };
+}
+
+function getInfo() {
+  return {
+    identity_pubkey: '036dfd60929cb57836a65daa763ceb36a26f4691c670fca91f9ee8b9bf2b445fb8',
+    alias: 'nicks-node',
+    num_pending_channels: 0,
+    num_active_channels: 0,
+    num_peers: 0,
+    block_height: 1382511,
+    block_hash: '0000000000000068cc4f6dccdd7efeecd718a19217025205515d3b3a898370c6',
+    syncedToChain: false,
+    testnet: true,
+    chains: [
+      'bitcoin'
+    ],
+    uris: ['036dfd60929cb57836a65daa763ceb36a26f4691c670fca91f9ee8b9bf2b445fb8:192.168.0.2:10009'],
+    best_header_timestamp: '1533778315',
+    version: '0.4.2-beta commit=33a5567a0fef801800cd56267e2b264d32c93173'
+  };
+}
+
+function getWalletBalance() {
+  return {
+    balance: '100000',
+    pendingOpenBalance: '150000'
+  };
+}
+
+function getManagedChannelsFile() {
+  return '{"6efe84b44bc9d184979f2527b73cbf0223a5549a3932e78a1460499166f2639e:0":{"name":"Test Node","purpose":"Much Lightning"}}';
+}
+
+function getOpenChannels() {
+  return [
+    {
+      active: true,
+      remotePubkey: '03311aebc4d9eb8a237d89ae771dec0d1b8a64aa31625c105800fdf5f934d824d2',
+      channelPoint: 'a6997a3b054265acb1a05e84f1b49f34e87a4758ea9b629839fe7311a0ac3c94:0',
+      chanId: '440904162803712',
+      capacity: '100000',
+      localBalance: '40950',
+      remoteBalance: '50000',
+      commitFee: '9050',
+      commitWeight: '724',
+      feePerKw: '12500',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [],
+      csvDelay: 144,
+      private: false
+    },
+    {
+      active: true,
+      remotePubkey: '03ae183a60b52ac3236c996ffbc0394423024e9e55ed43a3ebf245e26f09ca047b',
+      channelPoint: '47e615ba7d35b5c7e93a62e9adb84fddc11df43dc0790b7000a0a42be243e210:0',
+      chanId: '447501232570368',
+      capacity: '20000',
+      localBalance: '10950',
+      remoteBalance: '0',
+      commitFee: '9050',
+      commitWeight: '600',
+      feePerKw: '12500',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [],
+      csvDelay: 144,
+      private: false
+    }];
+}
+
+function getPendingChannels() {
+  return {
+    total_limbo_balance: '0',
+    pendingOpenChannels: [
+      {
+        channel: {
+          remoteNodePub: '03a13a469bae4785e27fae24e7664e648cfdb976b97f95c694dea5e55e7d302846',
+          channelPoint: 'c0b7045595f4f5c024af22312055497e99ed8b7b62b0c7e181d16382a07ae58b:0',
+          capacity: '10000000',
+          localBalance: '9999817',
+          remoteBalance: '0'
+        },
+        confirmationHeight: 0,
+        commitFee: '183',
+        commitWeight: '600',
+        feePerKw: '253'
+      },
+      {
+        channel: {
+          remoteNodePub: '03a13a469bae4785e27fae24e7664e648cfdb976b97f95c694dea5e55e7d302846',
+          channelPoint: 'c1b7045595f4f5c024af22287755b21f65e1ec7fbe11ee0181d16382a07ae58b:0',
+          capacity: '10000000',
+          localBalance: '9999817',
+          remoteBalance: '0'
+        },
+        confirmationHeight: 0,
+        commitFee: '183',
+        commitWeight: '600',
+        feePerKw: '253'
+      }
+    ],
+    pendingClosingChannels: [],
+    pendingForceClosingChannels: [],
+    waitingCloseChannels: []
+  };
+}
+
+function getOnChainTransactions() {
+  return [
+    {
+      active: true,
+      remotePubKey: '0270685ca81a8e4d4d01beec5781f4cc924684072ae52c507f8ebe9daf0caaab7b',
+      channelPoint: '9449c2cba3cb9a94bad58eeff3287755b21f65e1ec7fbe11ee0f485a6bb4094e:0',
+      chanId: '1582956994904784896',
+      capacity: '10000000',
+      localBalance: '9739816',
+      remoteBalance: '260000',
+      commitFee: '184',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '260000',
+      totalSatoshisReceived: '0',
+      numUpdates: '10',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false
+    },
+    {
+      active: true,
+      remotePubKey: '036b96e4713c5f84dcb8030592e1bd42a2d9a43d91fa2e535b9bfd05f2c5def9b9',
+      channelPoint: '2786816bc527ec570c6fd249ce85fa4e6bddc70675b6a03f1a4a5eefaaae3663:0',
+      chanId: '1582956994904915968',
+      capacity: '10000000',
+      localBalance: '9999817',
+      remoteBalance: '0',
+      commitFee: '183',
+      commitWeight: '600',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false
+    },
+    {
+      active: true,
+      remotePubKey: '03819ddbe246214d4c57b7ea4d28bfe5c09c03bb4308b40c26f1468532131e0cc0',
+      channelPoint: 'bea04831d2f479de97a08cd12af688e930eadf2e470e7e6c1719cdf4d5982114:0',
+      chanId: '1582956994904719360',
+      capacity: '10000000',
+      localBalance: '9999817',
+      remoteBalance: '0',
+      commitFee: '183',
+      commitWeight: '600',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false
+    },
+    {
+      active: true,
+      remotePubKey: '03adf1a17ab83438f23bc6c3b87ed8664757923988d5907c469840ddba1a7d1415',
+      channelPoint: 'da6d80297ec79cf115140c4272a4e07b9c275bdd0692b85b3167c58b8c556328:0',
+      chanId: '1582956994904850432',
+      capacity: '10000000',
+      localBalance: '9999817',
+      remoteBalance: '0',
+      commitFee: '183',
+      commitWeight: '600',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false
+    },
+    {
+      active: false,
+      remotePubKey: '03c856d2dbec7454c48f311031f06bb99e3ca1ab15a9b9b35de14e139aa663b463',
+      channelPoint: '12d3f818e82f448f780539c3b51616c23bc739f2b18bb8f6838200b111230d0e:0',
+      chanId: '1583392401509449728',
+      capacity: '3999000',
+      localBalance: '2000000',
+      remoteBalance: '1998817',
+      commitFee: '183',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 480,
+      private: false
+    },
+    {
+      active: true,
+      remotePubKey: '03c856d2dbec7454c48f311031f06bb99e3ca1ab15a9b9b35de14e139aa663b463',
+      channelPoint: '6efe84b44bc9d184979f2527b73cbf0223a5549a3932e78a1460499166f2639e:0',
+      chanId: '1582997676835799040',
+      capacity: '15000000',
+      localBalance: '14259822',
+      remoteBalance: '739994',
+      commitFee: '184',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '1500000',
+      totalSatoshisReceived: '760005',
+      numUpdates: '16',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1802,
+      private: false
+    },
+    {
+      active: true,
+      remotePubKey: '03c856d2dbec7454c48f311031f06bb99e3ca1ab15a9b9b35de14e139aa663b463',
+      channelPoint: 'c1b7045595f4f5c024af22287755b21f65e1ec7fbe11ee0181d16382a07ae58b:0',
+      chanId: '1582997676835799040',
+      capacity: '15000000',
+      localBalance: '14259822',
+      remoteBalance: '739994',
+      commitFee: '184',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '1500000',
+      totalSatoshisReceived: '760005',
+      numUpdates: '16',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1802,
+      private: false
+    }
+  ];
+}
+
+function getReversedOnChainTransactions() {
+  return [
+    {
+      active: true,
+      remotePubKey: '03c856d2dbec7454c48f311031f06bb99e3ca1ab15a9b9b35de14e139aa663b463',
+      channelPoint: 'c1b7045595f4f5c024af22287755b21f65e1ec7fbe11ee0181d16382a07ae58b:0',
+      chanId: '1582997676835799040',
+      capacity: '15000000',
+      localBalance: '14259822',
+      remoteBalance: '739994',
+      commitFee: '184',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '1500000',
+      totalSatoshisReceived: '760005',
+      numUpdates: '16',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1802,
+      private: false,
+      type: 'UNKNOWN'
+    },
+    {
+      active: true,
+      remotePubKey: '03c856d2dbec7454c48f311031f06bb99e3ca1ab15a9b9b35de14e139aa663b463',
+      channelPoint: '6efe84b44bc9d184979f2527b73cbf0223a5549a3932e78a1460499166f2639e:0',
+      chanId: '1582997676835799040',
+      capacity: '15000000',
+      localBalance: '14259822',
+      remoteBalance: '739994',
+      commitFee: '184',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '1500000',
+      totalSatoshisReceived: '760005',
+      numUpdates: '16',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1802,
+      private: false,
+      type: 'UNKNOWN'
+    },
+    {
+      active: false,
+      remotePubKey: '03c856d2dbec7454c48f311031f06bb99e3ca1ab15a9b9b35de14e139aa663b463',
+      channelPoint: '12d3f818e82f448f780539c3b51616c23bc739f2b18bb8f6838200b111230d0e:0',
+      chanId: '1583392401509449728',
+      capacity: '3999000',
+      localBalance: '2000000',
+      remoteBalance: '1998817',
+      commitFee: '183',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 480,
+      private: false,
+      type: 'UNKNOWN'
+    },
+    {
+      active: true,
+      remotePubKey: '03adf1a17ab83438f23bc6c3b87ed8664757923988d5907c469840ddba1a7d1415',
+      channelPoint: 'da6d80297ec79cf115140c4272a4e07b9c275bdd0692b85b3167c58b8c556328:0',
+      chanId: '1582956994904850432',
+      capacity: '10000000',
+      localBalance: '9999817',
+      remoteBalance: '0',
+      commitFee: '183',
+      commitWeight: '600',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false,
+      type: 'UNKNOWN'
+    },
+    {
+      active: true,
+      remotePubKey: '03819ddbe246214d4c57b7ea4d28bfe5c09c03bb4308b40c26f1468532131e0cc0',
+      channelPoint: 'bea04831d2f479de97a08cd12af688e930eadf2e470e7e6c1719cdf4d5982114:0',
+      chanId: '1582956994904719360',
+      capacity: '10000000',
+      localBalance: '9999817',
+      remoteBalance: '0',
+      commitFee: '183',
+      commitWeight: '600',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false,
+      type: 'UNKNOWN'
+    },
+    {
+      active: true,
+      remotePubKey: '036b96e4713c5f84dcb8030592e1bd42a2d9a43d91fa2e535b9bfd05f2c5def9b9',
+      channelPoint: '2786816bc527ec570c6fd249ce85fa4e6bddc70675b6a03f1a4a5eefaaae3663:0',
+      chanId: '1582956994904915968',
+      capacity: '10000000',
+      localBalance: '9999817',
+      remoteBalance: '0',
+      commitFee: '183',
+      commitWeight: '600',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '0',
+      totalSatoshisReceived: '0',
+      numUpdates: '0',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false,
+      type: 'UNKNOWN'
+    },
+    {
+      active: true,
+      remotePubKey: '0270685ca81a8e4d4d01beec5781f4cc924684072ae52c507f8ebe9daf0caaab7b',
+      channelPoint: '9449c2cba3cb9a94bad58eeff3287755b21f65e1ec7fbe11ee0f485a6bb4094e:0',
+      chanId: '1582956994904784896',
+      capacity: '10000000',
+      localBalance: '9739816',
+      remoteBalance: '260000',
+      commitFee: '184',
+      commitWeight: '724',
+      feePerKw: '253',
+      unsettledBalance: '0',
+      totalSatoshisSent: '260000',
+      totalSatoshisReceived: '0',
+      numUpdates: '10',
+      pendingHtlcs: [
+      ],
+      csvDelay: 1201,
+      private: false,
+      type: 'UNKNOWN'
+    },
+  ];
+}
+
+function getUnspectUtxos() {
+  return {
+    utxos: [
+      {
+        address_type: 0,
+        address: 'bc1qp6dxazrsju834sucudvcxy6t3tem3henkf9dfe',
+        amount_sat: 167288,
+        pk_script: '00140e9a6e8870970f1ac398e35983134b8af3b8df33',
+        outpoint: '75d342c126473f2bc26c23111b4cc4f8712532a0387d9bb70156d7efc8528efd:1',
+        confirmations: 51
+      },
+      {
+        address_type: 0,
+        address: 'bc1qp6dxazrsju834sucudvcxy6t3tem3henkf9dfe',
+        amount_sat: 44116,
+        pk_script: '00140e9a6e8870970f1ac398e35983134b8af3b8df33',
+        outpoint: '75d342c126473f2bc26c23111b4cc4f8712532a0387d9bb70156d7efc8528efd:1',
+        confirmations: 3307
+      },
+      {
+        address_type: 0,
+        address: 'bc1qp6dxazrsju834sucudvcxy6t3tem3henkf9dfe',
+        amount_sat: 30094,
+        pk_script: '00140e9a6e8870970f1ac398e35983134b8af3b8df33',
+        outpoint: '75d342c126473f2bc26c23111b4cc4f8712532a0387d9bb70156d7efc8528efd:1',
+        confirmations: 3996
+      },
+      {
+        address_type: 0,
+        address: 'bc1qp6dxazrsju834sucudvcxy6t3tem3henkf9dfe',
+        amount_sat: 185046,
+        pk_script: '00140e9a6e8870970f1ac398e35983134b8af3b8df33',
+        outpoint: '75d342c126473f2bc26c23111b4cc4f8712532a0387d9bb70156d7efc8528efd:1',
+        confirmations: 26309
+      },
+      {
+        address_type: 1,
+        address: '2NFGwqm9N9LomEh9mzQgofr1WGqkwaxPuWg',
+        amount_sat: 159770,
+        pk_script: '00140e9a6e8870970f1ac398e35983134b8af3b8df33',
+        outpoint: '75d342c126473f2bc26c23111b4cc4f8712532a0387d9bb70156d7efc8528efd:1',
+        confirmations: 27335
+      }
+    ]
+  };
+}
+
+module.exports = {
+  generateAddress,
+  getChannelBalance,
+  getEstimateFee,
+  getForwardingEvents,
+  getInfo,
+  getWalletBalance,
+  getOpenChannels,
+  getOnChainTransactions,
+  getPendingChannels,
+  getManagedChannelsFile,
+  getReversedOnChainTransactions,
+  getUnspectUtxos,
+};
